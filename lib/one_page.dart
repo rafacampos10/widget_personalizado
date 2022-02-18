2f717db01301593b294c386883447e4615e43b6c
Rafa Campos <rafinhasep94@hotmail.com>
2022-02-18 10:08:08 -0300

    primeira versão

---
+++ lib/one_page.dart
@@ -0,0 +1,17 @@
+import "package:flutter/material.dart";
+
+    class OnePage extends StatelessWidget {
+      const OnePage({Key? key}) : super(key: key);
+
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          body: Center(
+            child: ElevatedButton(
+              onPressed: (){},
+              child: Text("Botão"),
+            ),
+          ),
+        );
+      }
+    }
