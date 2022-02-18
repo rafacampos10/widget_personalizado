dabd4a66588a5d843761faba7601871dcbc4d6e9
Rafa Campos <rafinhasep94@hotmail.com>
2022-02-18 16:28:43 -0300

    Incrementando o botão random

--- lib/one_page.dart
+++ lib/one_page.dart
@@ -1,20 +1,53 @@
+import 'dart:math';
+
 import "package:flutter/material.dart";
 import 'package:widget_personalizado/widgets/custom_buttom_widget.dart';
 
-    class OnePage extends StatelessWidget {
-      const OnePage({Key? key}) : super(key: key);
+class OnePage extends StatefulWidget {
+  const OnePage({Key? key}) : super(key: key);
 
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-          body: Center(
-            child: CustomButtomWidget(
-              disable: false,
-              onPressed: (){},
-              title: "Custom BTN",
-              titleSize: 15,
-            ),
-          ),
-        );
-      }
+  @override
+  _OnePageState createState() => _OnePageState();
+}
+
+class _OnePageState extends State<OnePage> {
+
+  ValueNotifier <int> valorAleatorio = ValueNotifier <int> (0);
+
+  random() async{
+    for(int i = 0; i<10; i++){
+      await Future.delayed(Duration(seconds: 1));
+        valorAleatorio.value = Random().nextInt(1000);
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("build");
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ValueListenableBuilder(
+                valueListenable: valorAleatorio,
+                builder: (_,value,__) => Text(
+                  "Valor é: $value",
+                  style: TextStyle(fontSize: 20),
+                ),
+            ),
+            SizedBox(height: 10),
+            CustomButtomWidget(
+          disable: false,
+          onPressed: () => random(),
+          title: "Custom BTN",
+          titleSize: 15,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
